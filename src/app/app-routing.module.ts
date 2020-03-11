cf76fe879de8924262230b62fd49bb8238c289c2
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-03-11 12:59:44 -0300

    Initial deployment - comps and styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component'
+import { CountryComponent } from './country/country.component'
+import { AboutComponent } from './about/about.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'country', component: CountryComponent },
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
